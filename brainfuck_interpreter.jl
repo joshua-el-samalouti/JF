e47227c54f78e5d2b715dce9435a35d3daa58fb2
Joshua El-Samalouti <74884709+joshua-el-samalouti@users.noreply.github.com>
2026-06-03 21:35:41 +0200

    Update brainfuck_interpreter.jl

--- brainfuck_interpreter.jl
+++ brainfuck_interpreter.jl
@@ -1,15 +1,22 @@
 # Brainfuck Interpreter
 
+# output_mode "ascii" or "numeric"
 function brainfuck_interpreter(path, output_mode)
-    # output_mode "ascii" or "numeric"
     open(path, "r") do file
         script = read(file, String)
 
+        # BF bytearray is represented as a dictionary with indices as keys and bytes as values
         bytearray = Dict(
             1 => 0
         )
+
+        # pointer variable represents index of currently selected byte
         pointer = 1
+
+        # index variable represents index of command in the script
         index = 1
+
+        
         while index <= length(script)
             if script[index] == '+'
                 if bytearray[pointer] == 255
