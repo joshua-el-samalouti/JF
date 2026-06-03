bcd268d907a8c88ec9cab9cb032e11fa6543ca30
Joshua El-Samalouti <74884709+joshua-el-samalouti@users.noreply.github.com>
2026-06-03 21:29:24 +0200

    Update brainfuck_run.jl

--- brainfuck_run.jl
+++ brainfuck_run.jl
@@ -1,3 +1,5 @@
+# Runs brainfuck_testscript.bf located in the same folder as this file
+
 include("brainfuck_interpreter.jl")
 dir = @__DIR__
 brainfuck_interpreter(dir * "/brainfuck_testscript.bf", "ascii")
