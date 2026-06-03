d5f33a8499ec41fc5f65c23b9c14afeb400e9696
Joshua El-Samalouti <74884709+joshua-el-samalouti@users.noreply.github.com>
2026-06-03 23:04:37 +0200

    Update brainfuck_interpreter.jl

--- brainfuck_interpreter.jl
+++ brainfuck_interpreter.jl
@@ -16,30 +16,39 @@ function brainfuck_interpreter(path, output_mode)
         # index variable represents index of command in the script
         index = 1
 
-        
         while index <= length(script)
+
+            # + command increments currently selected byte by 1. Overflow is simulated.
             if script[index] == '+'
                 if bytearray[pointer] == 255
                     bytearray[pointer] = 0
                 else
                     bytearray[pointer] = bytearray[pointer] + 1
                 end
+
+            # - command decrements currently selected byte by 1. Underflow is simulated.
             elseif script[index] == '-'
                 if bytearray[pointer] == 0
                     bytearray[pointer] = 255
                 else
                     bytearray[pointer] = bytearray[pointer] - 1
                 end
+
+            # > command increases the index pointer by 1. If there is no key in the bytearray for the newly selected byte, it is added.
             elseif script[index] == '>'
                 pointer += 1
                 if !haskey(bytearray,pointer)
                     bytearray[pointer] = 0
                 end
+
+            # < command decreases the index pointer by 1.  If there is no key in the bytearray for the newly selected byte, it is added.
             elseif script[index] == '<'
                 pointer -= 1
                 if !haskey(bytearray,pointer)
                     bytearray[pointer] = 0
                 end
+
+            # [ command skips the script index forward to matching depth ] command if currently selected byte is 0
             elseif script[index] == '['
                 if bytearray[pointer] == 0
                     depth = 1
@@ -52,6 +61,8 @@ function brainfuck_interpreter(path, output_mode)
                         end
                     end
                 end
+
+            # ] command skips the script index back to matching depth [ command if currently selected byte is not 0
             elseif script[index] == ']'
                 if bytearray[pointer] != 0
                     depth = 1
@@ -64,6 +75,8 @@ function brainfuck_interpreter(path, output_mode)
                         end
                     end
                 end
+
+            # . command, depending on output mode, prints either numeric value or ascii character corresponding to the value of the currently selected byte.
             elseif script[index] == '.'
                 if output_mode == "ascii"
                     print(Char(bytearray[pointer]))
